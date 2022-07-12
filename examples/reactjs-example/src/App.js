52f247a6a9276d9b0f6703d0612a25ad01312f9a
hebertcisco <hebertcisco@outlook.com>
2022-07-12 20:44:40 -0300

    refactor: :rocket: add examples

--- examples/reactjs-example/src/App.js
+++ examples/reactjs-example/src/App.js
@@ -1,22 +1,22 @@
+import React from 'react';
+import { useOs } from 'use-os';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const current_os = useOs();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You are using{' '}
+          <a className="App-link" href={`#${current_os}`} target="_blank" rel="noopener noreferrer">
+            {current_os}
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
